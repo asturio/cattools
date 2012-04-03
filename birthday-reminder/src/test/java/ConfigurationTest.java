593afa051f11a8c0d70520910619a76da781ceff
asturio <asturio@e4eb239c-6f73-4cb8-adde-fa0f335d357c>
2012-04-03 19:38:29 +0000

    Files moves from old repo

---
+++ birthday-reminder/src/test/java/ConfigurationTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ConfigurationTest {
+    @Test
+    public static void testConfigurationTest() {
+        Configuration config = new Configuration();
+        config.
+    }
+}
