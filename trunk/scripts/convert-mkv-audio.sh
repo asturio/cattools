cfebb0daacd5faa581acb60be5cb21c5aed4251a
asturio <asturio@e4eb239c-6f73-4cb8-adde-fa0f335d357c>
2014-03-27 20:12:23 +0000

    Added gensig and modified the mkv-audio conversion to use better lib

--- trunk/scripts/convert-mkv-audio.sh
+++ trunk/scripts/convert-mkv-audio.sh
@@ -2,5 +2,5 @@
 
 FILE=$1
 
-# avconv -i "$FILE" -c:v copy -c:a libfaac -qscale:a 74 "0new-$FILE"
-avconv -i "$FILE" -c:v copy -c:a libvo_aacenc -b:a 112k "0new-112k-$FILE"
+avconv -i "$FILE" -c:v copy -c:a libfaac -q:a 74 "0new-$FILE"
+#avconv -i "$FILE" -c:v copy -c:a libvo_aacenc -b:a 112k "0new-112k-$FILE"
